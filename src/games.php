48171631a5ed73fe477c028452157d2a72820adf
asp73rus@mail.ru <asp73rus@mail.ru>
2019-07-07 19:00:24 +0400

    update

--- src/games.php
+++ src/games.php
@@ -11,25 +11,4 @@ function gameStart($game)
             $result = $value % 2 == 0 ? 'yes' : 'no';
             return [$value, $result];
     }
-    
-    
-
-    /*
-    if ($game == 'brain-even') {
-        $endGame = 0;
-        while ($endGame < 3) {
-            $num = rand(1, 100);
-            line("Question: {$num}");
-            $result = $num % 2 == 0 ? 'yes' : 'no';
-            $answer = strtolower(\cli\prompt('Your answer'));
-            if ($answer != $result) {
-                line("'{$answer}' is wrong answer ;(. Correct answer was '{$result}'.");
-                return line("Let's try again, {$name}!");
-            }
-            line('Correct!');
-            $endGame += 1;
-        }
-        line("Congratulations, {$name}!");
-    }
-    */
 }
