57653715441cf5a5c28cc8eed386ac530fece5c5
asp73rus@mail.ru <asp73rus@mail.ru>
2019-07-07 15:10:12 +0400

    update function

--- src/cli.php
+++ src/cli.php
@@ -1,12 +1,38 @@
 <?php
 namespace BrainGames\Cli;
 
-use function \cli\line;
+use function cli\line;
+use function BrainGames\Games\gameStart;
 
-function run()
+function greeting()
 {
     line('Welcome to the Brain Game!');
-    line();
+}
+
+function toAskName()
+{
     $name = \cli\prompt('May I have your name?');
+    return $name;
+}
+
+function sayHello($name)
+{
     line("Hello, %s!", $name);
+}
+
+function run($game = '')
+{
+    if ($game == 'brain-even') {
+        greeting();
+        line('Answer "yes" if number even otherwise answer "no".');
+        line();
+        $name = toAskName();
+        sayHello($name);
+        gameStart($game, $name);
+    } else {
+        greeting();
+        line();
+        $name = toAskName();
+        sayHello($name);
+    }
 }
